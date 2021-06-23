4d87f1deefb72cea35b3892170c7d2b332af6b9b
IrvingAguilera <79335314+IrvingAguilera@users.noreply.github.com>
2021-06-23 16:30:19 -0500

    Add files via upload

---
+++ Practica 3.13/ejemplo1.php
@@ -0,0 +1,6 @@
+<?php
+
+$edades = array("Pedro"=>35, "Benito"=>37, "Joel"=>43);
+
+$mijson = json_encode($edades);
+echo $mijson;
---
+++ Practica 3.13/ejemplo2.php
@@ -0,0 +1,4 @@
+<?php
+$autos = array("Volvo", "BMW", "Toyota");
+$mijson = json_encode($autos);
+echo $mijson;
